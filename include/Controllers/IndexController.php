3561a2b1d52a0a76811f4c0984781cdabdda733c
Dai Bui <daib@eecs.berkeley.edu>
2013-10-16 00:09:19 -0700

    view blog of specific users

--- include/Controllers/IndexController.php
+++ include/Controllers/IndexController.php
@@ -1,8 +1,26 @@
 <?php
     class IndexController extends CustomControllerAction
     {
+        public function init()
+        {
+            parent::init();
+            $this->breadcrumbs->addStep('Account', $this->getUrl(null, 'account'));
+            $this->breadcrumbs->addStep('Blog Manager',
+                                        $this->getUrl(null, 'blogmanager'));
+
+            $this->identity = Zend_Auth::getInstance()->getIdentity();
+        }
+
         public function indexAction()
         {
+            $uid = (int) $this->getRequest()->getQuery('uid');
+            if($uid == null && $this->identity !=  null)
+                $uid = $this->identity->user_id;
+            if($uid != null) {
+                $bl = new DatabaseObject_BlogList($this->db, $uid);
+                $full_name = $bl->getUserFirstName(). ' ' . $bl->getUserLastName();
+                $this->view->assign('full_name', $full_name);
+            }
         }
     }
 ?>
